8c5d542e0ff8076018266999eb3dab3ff3c5f4a6
csklimowski <csklimowski@email.arizona.edu>
2017-10-17 19:43:14 -0700

    removed map creation from state

--- src/states/main.js
+++ src/states/main.js
@@ -1,4 +1,5 @@
 import game from '../game';
+import buildLevelMap from '../util/map';
 
 export class MainState extends Phaser.State {
 	create() {
@@ -11,107 +12,27 @@ export class MainState extends Phaser.State {
 			fill: '#000000'
 		});
 
-		this.map = [];
-		for (let y = 0; y < 11; y++) {
-			let row = [];
-			for (let x = 0; x < 16; x++) {
-				row.push({
-					x, y,
-					realX: this.displayX(x),
-					realY: this.displayY(y),
-					type: 0
-				});
-			}
-			this.map.push(row);
-		}
+		let mapSrc = [
+			'          ',
+			' 00000000 ',
+			' 0n010010 ',
+			' 00000200 ',
+			'          '
+		];
 
-		let currentX = 7, currentY = 5, nextX = 7, nextY = 5, squares = 50;
-		while (squares > 0) {
-			let direction = Math.floor(Math.random() * 4);
-			while (this.map[nextY][nextX].type !== 0) {
-				if (direction === 0 && nextX < this.map[nextY].length - 2) {
-					nextX++;
-				} else if (direction === 1 && nextY < this.map.length - 2) {
-					nextY++;
-				} else if (direction === 2 && nextX > 2) {
-					nextX--;
-				} else if (direction === 3 && nextY > 2) {
-					nextY--;
-				} else {
-					break;
-				}
-			}
-			this.map[nextY][nextX].type = 1;
-			currentX = nextX;
-			currentY = nextY;
-			squares--;
-		}
-
-		for (let y = 1; y < this.map.length - 1; y++) {
-			for (let x = 1; x < this.map[y].length - 1; x++) {
-				if (this.map[y][x].type === 1 && Math.random() > 0.5) {
-					let adjacent = 0;
-					for (let y2 = -1; y2 < 2; y2++) {
-						for (let x2 = -1; x2 < 2; x2++) {
-							if (this.map[y+y2][x+x2].type === 1) {
-								adjacent++;
-							}
-						}
-					}
-					if (adjacent > 7) {
-						this.map[y][x].type = 4;
-					} else if (adjacent > 6) {
-						this.map[y][x].type = 3;
-					} else if (adjacent > 5) {
-						this.map[y][x].type = 2;
-					}
-				}
-			}
-		}
-
-		for (let y = 1; y < this.map.length - 1; y++) {
-			for (let x = 1; x < this.map[y].length - 1; x++) {
-				let adjacent = 0;
-				for (let y2 = -1; y2 < 2; y2++) {
-					for (let x2 = -1; x2 < 2; x2++) {
-						if (this.map[y+y2][x+x2].type !== 0) {
-							adjacent++;
-						}
-					}
-				}
-				if (adjacent < 3 && currentX !== x && currentY !== y) {
-					this.map[y][x].type = 0;
-				}
-			}
-		}
-
-		for (let y = this.map.length - 2; y > 0; y--) {
-			for (let x = this.map[y].length - 2; x > 0; x--) {
-				let adjacent = 0;
-				for (let y2 = -1; y2 < 2; y2++) {
-					for (let x2 = -1; x2 < 2; x2++) {
-						if (this.map[y+y2][x+x2].type !== 0) {
-							adjacent++;
-						}
-					}
-				}
-				if (adjacent < 3 && currentX !== x && currentY !== y) {
-					this.map[y][x].type = 0;
-				}
-			}
-		}
+		this.map = buildLevelMap(mapSrc);
 
 		this.player = {
-			x: currentX,
-			y: currentY,
-			nextX: currentX,
-			nextY: currentY,
+			x: this.map.startX,
+			y: this.map.startY,
+			nextX: this.map.startX,
+			nextY: this.map.startY,
 			charge: 0
 		};
 
 		this.path = [{
-			x: currentX,
-			y: currentY
+			x: this.map.startX,
+			y: this.map.startY
 		}];
 		
 		this.calculateValidMoves();
@@ -152,7 +73,7 @@ export class MainState extends Phaser.State {
 		this.validMoves = [];
 		for (let x = -1; x < 2; x++) {
 			for (let y = -1; y < 2; y++) {
-				if (!(x === 0 && y === 0) && this.map[this.player.y + y][this.player.x + x].type !== 0) {
+				if (!(x === 0 && y === 0) && this.map[this.player.y + y][this.player.x + x] !== null) {
 					let valid = true;
 					for (let i = 0; i < this.path.length - 1; i++) {
 						if (this.path[i].x   === this.player.x     && this.path[i].y   === this.player.y &&
@@ -192,14 +113,6 @@ export class MainState extends Phaser.State {
 		for (let y = 0; y < this.map.length; y++) {
 			for (let x = 0; x < this.map[y].length; x++) {
 				if (this.map[y][x].type !== 0) {
-					// if (x < this.map[y].length - 1 && this.map[y][x+1].type !== 0) {
-					// 	this.graphics.moveTo(this.map[y][x].realX, this.map[y][x].realY);
-					// 	this.graphics.lineTo(this.map[y][x+1].realX, this.map[y][x+1].realY);
-					// }
-					// if (y < this.map.length - 1 && this.map[y+1][x].type !== 0) {
-					// 	this.graphics.moveTo(this.map[y][x].realX, this.map[y][x].realY);
-					// 	this.graphics.lineTo(this.map[y+1][x].realX, this.map[y+1][x].realY);
-					// }
 					if (this.map[y][x].type === 1) {
 						this.graphics.beginFill(0xaaaaaa);
 						this.graphics.drawCircle(this.map[y][x].realX, this.map[y][x].realY, 10);
