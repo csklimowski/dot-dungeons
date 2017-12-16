bfd2d0459cbbf6e48973d1f35a2fbd76256f459d
Chris Klimowski <csklimowski@gmail.com>
2017-12-15 20:29:56 -0700

    new levels

--- src/levels/world1.js
+++ src/levels/world1.js
@@ -3,116 +3,160 @@ export const world1 = [
         id: '1-A',
         name: 'Where Is Everyone?',
         layout: [
-            '          ',
-            '   00000   ',
-            '   00000   ',
-            ' N0001000X ',
-            '   00000   ',
-            '   00000   ',
-            '          ',
+            '         ',
+            '  00000  ',
+            '  00000  ',
+            ' N00100X ',
+            '  00000  ',
+            '  00000  ',
+            '         ',
         ],
-        prereqs: []
     },
     {
         id: '1-B',
         name: 'Oh There They Are',
         layout: [
-            '          ',
-            '   00000   ',
-            '   01110   ',
-            ' N0011100X ',
-            '   01110   ',
-            '   00000   ',
-            '          ',
-        ],
-        prereqs: [
-            '1-A'
+            '         ',
+            '  10101  ',
+            '  01110  ',
+            ' N11111X ',
+            '  01110  ',
+            '  10101  ',
+            '         ',
         ]
     },
     {
         id: '2-A',
         name: 'Closing In',
         layout: [
-            '          ',
-            '   0000   ',
-            ' N001100X ',
-            '   0000   ',
-            '          ',
-        ],
-        prereqs: [
-            '1-A'
+            '        ',
+            '  0000  ',
+            ' N0110X ',
+            '  0000  ',
+            '        ',
         ]
     },
     {
         id: '2-B',
         name: 'Blockade',
         layout: [
-            '          ',
-            '   0110   ',
-            ' N001100X ',
-            '   0110   ',
-            '          ',
-        ],
-        prereqs: [
-            '2-A'
+            '        ',
+            '  0111  ',
+            ' N0111X ',
+            '  0111  ',
+            '        ',
         ]
     },
     {
         id: '3-A',
         name: 'Hallway Fight',
         layout: [
-            '           ',
-            ' N0011100X ',
-            '   00000   ',
-            '           ',
-        ],
-        prereqs: [
-            '2-A'
+            '         ',
+            ' N01110X ',
+            '  00000  ',
+            '         ',
         ]
     },
     {
         id: '3-B',
         name: 'They Brought Friends',
         layout: [
-            '           ',
-            ' N0111110X ',
-            '   00000   ',
-            '           ',
-        ],
-        prereqs: [
-            '3-A'
+            '         ',
+            ' N11111X ',
+            '  00000  ',
+            '         ',
         ]
     },
     {
         id: '4-A',
-        name: 'Line Them Up',
+        name: 'Four Corners',
         layout: [
             '        ',
-            '   00   ',
-            '   01   ',
-            '   01   ',
-            '   01   ',
-            ' N0010X ',
-            '        ',
-        ],
-        prereqs: [
-            '2-A'
+            '  101  ',
+            ' N000X ',
+            '  101  ',
+            '        '
         ]
     },
     {
         id: '4-B',
-        name: 'Just One More',
+        name: 'The Letter C',
         layout: [
             '        ',
-            '   01   ',
-            '   01   ',
-            '   01   ',
-            '   01   ',
-            ' N0010X ',
-            '        ',
-        ],
-        prereqs: [
-            '3-A'
+            '  111  ',
+            ' N001X ',
+            '  111  ',
+            '        '
         ]
     },
+    {
+        id: '5-A',
+        name: 'Line Them Up',
+        layout: [
+            '      ',
+            '  00  ',
+            '  01  ',
+            '  01  ',
+            '  01  ',
+            ' N01X ',
+            '      ',
+        ]
+    },
+    {
+        id: '5-B',
+        name: 'Just One More',
+        layout: [
+            '      ',
+            '  01  ',
+            '  01  ',
+            '  01  ',
+            '  01  ',
+            ' N01X ',
+            '      ',
+        ]
+    },
+    {
+        id: '6-A',
+        name: 'Twice the Pride',
+        layout: [
+            '        ',
+            '   00   ',
+            ' N0002X ',
+            '   00   ',
+            '        ',
+        ]
+    },
+    {
+        id: '6-B',
+        name: 'Double the Fall',
+        layout: [
+            '        ',
+            '   00   ',
+            ' N0220X ',
+            '   00   ',
+            '        ',
+        ]
+    },
+    {
+        id: '7-A',
+        name: 'Two By Two',
+        layout: [
+            '        ',
+            '  0220  ',
+            ' N0000X ',
+            '  0220  ',
+            '        '
+        ]
+    },
+    {
+        id: '7-B',
+        name: 'One Problem',
+        layout: [
+            '        ',
+            '  0220  ',
+            ' N0110X ',
+            '  0220  ',
+            '        '
+        ]
+    }
 ];
