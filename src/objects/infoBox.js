920bfae161a6b9e0a4e53af5076e06dccef4c271
Chris Klimowski <csklimowski@gmail.com>
2018-10-09 23:09:52 -0700

    undo icon for tutorial

--- src/objects/infoBox.js
+++ src/objects/infoBox.js
@@ -7,15 +7,23 @@ export class InfoBox extends Phaser.Image {
         this.text = game.make.bitmapText(this.x + 40, this.y + 30, 'handwriting', '', 70);
         game.stage.addChild(this.text);
         this.targetY = this.y;
+        this.undoIcon = game.make.image(850, 0, 'undo');
+        game.stage.addChild(this.undoIcon);
     }
 
     update() {
         let dt = game.time.elapsedMS / 1000;
         this.y += 10*(this.targetY - this.y)*dt;
         this.text.y = this.y + 30;
+        this.undoIcon.y = this.y + 32;
     }
     
     appear(text) {
+        if (text.indexOf('    ') !== -1) {
+            this.undoIcon.alpha = 1;
+        } else {
+            this.undoIcon.alpha = 0;
+        }
         this.text.text = text;
         this.targetY = 450;
     }
